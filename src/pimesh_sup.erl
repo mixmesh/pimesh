7a553625821369fb77501f872a25db83919c2554
Tony Rogvall <tony@rogvall.se>
2021-03-25 21:54:52 +0100

    select hardware

--- src/pimesh_sup.erl
+++ src/pimesh_sup.erl
@@ -28,16 +28,25 @@ init(_Args) ->
     AppServ =
 	{pimesh_app_serv, {pimesh_app_serv, start_link, []},
 	 permanent, 5000, worker, [pimesh_app_serv]},
-    BatteryServ = 
-	{pimesh_battery_serv, {pimesh_battery_serv, start_link, []},
-	 permanent, 5000, worker, [pimesh_battery_serv]},
-    GpsServ = 
-	{pimesh_gps_serv, {pimesh_gps_serv, start_link, []},
-	 permanent, 5000, worker, [pimesh_gps_serv]},
-    PiMeshServ =
-	{pimesh_serv, {pimesh_serv, start_link, []},
-	 permanent, 5000, worker, [pimesh_serv]},
+    ServList = case config:lookup([system,hardware], none) of
+		   pimesh ->
+		       [{pimesh_gps_serv, {pimesh_gps_serv, start_link, []},
+			 permanent, 5000, worker, [pimesh_gps_serv]},
 
-    {ok,{{one_for_all,3,5}, [AppServ,BatteryServ,GpsServ,PiMeshServ]}}.
+			{pimesh_battery_serv, 
+			 {pimesh_battery_serv, start_link, []},
+			 permanent, 5000, worker, [pimesh_battery_serv]},
 
+			{pimesh_serv, {pimesh_serv, start_link, []},
+			 permanent, 5000, worker, [pimesh_serv]}];
+		   epxmesh ->
+		       [{sys_battery_serv, 
+			 {sys_battery_serv, start_link, []},
+			 permanent, 5000, worker, [sys_battery_serv]},
 
+			{epxmesh_serv, {epxmesh_serv, start_link, []},
+			 permanent, 5000, worker, [epxmesh_serv]}];
+		   none ->
+		       []
+	       end,
+    {ok,{{one_for_all,3,5}, [AppServ|ServList]}}.
